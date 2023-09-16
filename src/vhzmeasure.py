fc11f8097a85775c991440eef9afb1ce735bb1d3
yeranosyanvahan <yeranosyanvahan@gmail.com>
2023-09-16 14:31:02 +0400

    add correction

--- src/vhzmeasure.py
+++ src/vhzmeasure.py
@@ -28,7 +28,7 @@ def vhzmeasure(nsample = 1000):
         VT = np.array([(read_analog(CHANNEL), time.monotonic_ns()//1000) for _ in range(nsample)])
 
         MILISECOND = 1000
-        VOLT = 1
+        VOLT = 1.052
 
         V, T  = VT[5:-5].astype(int).T
         index = np.where( (V[2:] <= V[1:-1]) & (V[1:-1] > V [:-2]))[0]
